f22452566ef27dc55508d3615b93b1d3219eff11
KomaVR <komavr12454@gmail.com>
2024-12-09 11:18:43 -0800

    Update login.js

--- pages/api/login.js
+++ pages/api/login.js
@@ -4,19 +4,14 @@ const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY
 
 export default async function handler(req, res) {
   if (req.method === "POST") {
-    const { username, password } = req.body;
+    const { email, password } = req.body;
+    const { error } = await supabase.auth.signInWithPassword({ email, password });
 
-    const { data, error } = await supabase
-      .from("users")
-      .select("*")
-      .eq("username", username)
-      .eq("password", password);
-
-    if (data.length === 0) {
-      return res.status(400).json({ success: false, message: "Invalid credentials!" });
+    if (error) {
+      res.status(400).json({ message: error.message });
+    } else {
+      res.status(200).json({ message: "User logged in successfully" });
     }
-
-    res.status(200).json({ success: true });
   } else {
     res.status(405).json({ message: "Method not allowed" });
   }
